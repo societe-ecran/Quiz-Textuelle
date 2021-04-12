cc5ccfe62e5b5d797b43f2f0dd9bc269c82b664a
laz <laz@pop-os.localdomain>
2021-04-12 07:12:05 +0200

    sale

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+// import "./App.css";
+// import sal from "../node_modules/sal.js/dist/sal";
+import sal from 'sal.js'
+import "./style.scss";
 
 function App() {
+  sal()
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +22,245 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <div
+              data-sal='slide-up'
+              data-sal-delay="0"
+              data-sal-easing="ease"
+          
+            >
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+      </div>
+      <div data-sal="fade">
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+      </div>
+      <div data-sal="fade">
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+      </div>
+      <div
+        data-sal="slide-up"
+        data-sal-delay="300"
+        data-sal-easing="ease-out-back"
+      >
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      
+      fgbbgfbf <br />
+      </div>{" "}
+      <div
+        data-sal="slide-up"
+        data-sal-delay="300"
+        data-sal-easing="ease-out-back"
+      >
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      
+      fgbbgfbf <br />
+      </div>{" "}
+      <div
+        data-sal="slide-up"
+        data-sal-delay="300"
+        data-sal-easing="ease-out-back"
+      >
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      
+      fgbbgfbf <br />
+      </div>{" "}
+      <div
+        data-sal="slide-up"
+        data-sal-delay="300"
+        data-sal-easing="ease-out-back"
+      >
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      
+      fgbbgfbf <br />
+      </div>{" "}
+      <div
+        data-sal="slide-up"
+        data-sal-delay="300"
+        data-sal-easing="ease-out-back"
+      >
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      
+      fgbbgfbf <br />
+      </div>{" "}
+      <div
+        data-sal="slide-up"
+        data-sal-delay="300"
+        data-sal-easing="ease-out-back"
+      >
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      
+      fgbbgfbf <br />
+      </div>{" "}
+      <div
+        data-sal="slide-up"
+        data-sal-delay="300"
+        data-sal-easing="ease-out-back"
+      >
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      
+      fgbbgfbf <br />
+      </div>{" "}
+      <div
+        data-sal="slide-up"
+        data-sal-delay="300"
+        data-sal-easing="ease-out-back"
+      >
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      fgbbgfbf <br />
+      sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+      
+      fgbbgfbf <br />
+      </div>{" "}
+      <div data-sal="fade">
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+      </div>
+      <div data-sal="fade">
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+      </div>
+      <div data-sal="fade">
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+      </div>
+      <div data-sal="fade">
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+      </div>
+      <div data-sal="fade">
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+        sexdcfgvhbjnk,lknjbhvgcfxdwsdxfcgvhb <br />
+        fgbbgfbf <br />
+      </div>
     </div>
   );
 }
